aeec87e82e3e4117bf4ad0ee5bf680f9c63fde49
sean conroy <95358758+seanconroy2021@users.noreply.github.com>
2022-10-26 12:45:57 +0100

    Note Class 1.0
    The writing of the note class for the project today this is part 0.1

---
+++ untitled 2/src/models/Note.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Note {
+
+
+private String noteTitle ="No Title";
+private int notePriority =1;
+private  String noteCategory ="";
+private boolean isNoteArchived= false;
+
+private ArrayList<Item> items;
+
+
+    //constructor
+    public Note(String noteTitle, int notePriority, String noteCategory)
+    {
+
+
+    }
+
+
+
+
+
+
+}
